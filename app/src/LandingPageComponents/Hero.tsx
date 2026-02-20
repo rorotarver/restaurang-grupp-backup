8d34031dd46ea4dbc99f9bb3197e39bc825439fd
Equilibrium <miltos-05032006@hotmail.com>
2026-02-20 15:10:22 +0100

    added Navbar and Hero tsx

---
+++ app/src/LandingPageComponents/Hero.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export const Hero = () => {
+    return (
+        <section>
+            <div>
+                <h2>
+                    Autentisk Persisk Mat
+                </h2>
+                <p>
+                    Färska råvaror, traditionella recept och en varm atmosfär.
+                </p>
+                <button>
+                    Boka Här
+                </button>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
---
+++ app/src/LandingPageComponents/Navbar.tsx
@@ -0,0 +1,15 @@
+"use clietn";
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <h1>The Restaurant Fantastic Food</h1>
+            <div>
+                <a href="#about">Om oss</a>
+                <a href="#contact">Kontakt</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
