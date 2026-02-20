f4345f64cf03e535e65a67d4964f29f1b766468a
Equilibrium <miltos-05032006@hotmail.com>
2026-02-20 15:13:02 +0100

    added about tsx

---
+++ app/src/LandingPageComponents/About.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const About = () => {
+    return (
+        <section id="about">
+            <h3>Om oss</h3>
+            <p>detta är about</p>
+        </section>
+    );
+};
+
+export default About;
