ab07e88b611364836a73f422c3a8f421fb25a707
Equilibrium <miltos-05032006@hotmail.com>
2026-02-20 18:47:17 +0100

    added contact tsx and footer tsx

---
+++ app/src/LandingPageComponents/Contanct.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export const Contact = () => {
+    return (
+        <section id="contact">
+            <h3>Kontakt Oss</h3>
+            <p>Address</p>
+            <p>Telephon</p>
+            <p>Email</p>
+        </section>
+    );
+};
+
+export default Contact;
---
+++ app/src/LandingPageComponents/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const Footer = () => {
+    return (
+        <footer>
+            © {new Date().getFullYear()} Bella Vista. Alla rättigheter förbehållna.
+        </footer>
+    );
+};
+
+export default Footer;
