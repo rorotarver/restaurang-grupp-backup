6b6c79a79b4072fcff358a57fee61854297ad5f0
Equilibrium <miltos-05032006@hotmail.com>
2026-02-23 12:13:04 +0100

    added margin on Contact.tsx

--- app/src/LandingPageComponents/Contanct.tsx
+++ app/src/LandingPageComponents/Contanct.tsx
@@ -2,11 +2,11 @@
 
 export const Contact = () => {
     return (
-        <section id="contact">
-            <h3>Kontakt Oss</h3>
-            <p>Address</p>
-            <p>Telephon</p>
-            <p>Email</p>
+        <section id="contact" className="px-6 py-20 bg-white text-center">
+            <h3 className="text-3xl font-semibold mb-8">Kontakt Oss</h3>
+            <p className="mb-4">Address</p>
+            <p className="mb-4">Telephon</p>
+            <p className="mb-4">Email</p>
         </section>
     );
 };
