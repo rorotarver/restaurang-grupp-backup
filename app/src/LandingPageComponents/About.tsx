179464c4d90dea9ef49500ca06c2a7fddb2de096
Equilibrium <miltos-05032006@hotmail.com>
2026-02-23 12:10:27 +0100

    style on about

--- app/src/LandingPageComponents/About.tsx
+++ app/src/LandingPageComponents/About.tsx
@@ -2,9 +2,9 @@
 
 export const About = () => {
     return (
-        <section id="about">
-            <h3>Om oss</h3>
-            <p>detta är about</p>
+        <section id="about" className="px-6 py-20 bg-orange-50 text-center">
+            <h3 className="text-3xl font-semibold mb-6">Om oss</h3>
+            <p className="max-w-3xl max-auto text-gray-600">detta är about</p>
         </section>
     );
 };
--- app/src/LandingPageComponents/Hero.tsx
+++ app/src/LandingPageComponents/Hero.tsx
@@ -2,15 +2,15 @@
 
 export const Hero = () => {
     return (
-        <section>
-            <div>
-                <h2>
+        <section className="flex flex-col items-center justify-center text-center px-6 py-28 bg-cover bg-center" style={{ backgroundImage: "url('/app/src/img/2Q.png')" }}>
+            <div className="bg-black/50 p-10 rounded-2xl">
+                <h2 className="text-5xl font-bold text-white mb-6">
                     Autentisk Persisk Mat
                 </h2>
-                <p>
-                    Färska råvaror, traditionella recept och en varm atmosfär.
+                <p className="text-5xl font-bold text-white mb-6">
+                  Färska råvaror, traditionella recept och en varm atmosfär.
                 </p>
-                <button>
+                <button className="bg-orange-500 hover:bg-orange-600 text-black px-8 py-3 round-2xl shadow-lg transition">
                     Boka Här
                 </button>
             </div>
