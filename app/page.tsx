ae365f2c90e710d60ca6ccb4ce6afa1655e95a10
Equilibrium <miltos-05032006@hotmail.com>
2026-02-24 12:05:43 +0100

    ändrade knappen till Link istället

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import LandingPage from "./src/LandingPageComponents/LandingPage";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black min-h-screen bg-cover bg-center bg-no-repeat items-center justify-center" style={{ backgroundImage: `url(/2Q.png)` }}>
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-white/50 sm:items-start">
+
         <LandingPage />
       </main>
     </div>
--- app/src/LandingPageComponents/Hero.tsx
+++ app/src/LandingPageComponents/Hero.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import Link from "next/link";
+
 
 export const Hero = () => {
     return (
         <section className="flex flex-col items-center justify-center text-center px-6 py-28 bg-cover bg-center">
-            <div className="bg-black/50 p-10 rounded-2xl">
+            <div className="bg-white/70 p-10 rounded-2xl">
                 <h2 className="text-5xl font-bold text-white mb-6">
                     Autentisk Persisk Mat
                 </h2>
                 <p className="text-5xl font-bold text-white mb-6">
                   Färska råvaror, traditionella recept och en varm atmosfär.
                 </p>
-                <button className="bg-orange-500 hover:bg-orange-600 text-black px-8 py-3 round-2xl shadow-lg transition">
+
+                <Link href="/BookingPage" className="bg-orange-500 hover:bg-orange-600 text-black px-8 py-3 rounded-2xl shadow-lg transition">
                     Boka Här
-                </button>
+                </Link>
+
+
+
+
             </div>
         </section>
     );
